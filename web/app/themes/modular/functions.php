48aa7ee2343ba03f340cd715329c5b0607f6038c
Toni Fisler <mail@tonifisler.ch>
2017-02-12 12:50:49 +0100

    fix functions php

--- web/app/themes/modular/functions.php
+++ web/app/themes/modular/functions.php
@@ -27,12 +27,18 @@ class StarterSite extends TimberSite {
 		add_action( 'init', array( $this, 'register_post_types' ) );
         add_action( 'init', array( $this, 'register_taxonomies' ) );
         add_action( 'wp_enqueue_scripts', array( $this, 'register_assets' ) );
-        add_action( 'admin_menu', array( $this, 'remove_default_post_type' ) );
+        add_action( 'admin_menu', array( $this, 'remove_from_menu' ) );
 		parent::__construct();
 	}
 
-    function remove_default_post_type() {
-        remove_menu_page('edit.php');
+    function remove_from_menu() {
+        $user = wp_get_current_user();
+        if ( !in_array( 'administrator', (array) $user->roles ) ) {
+            remove_menu_page('edit.php');
+            remove_menu_page( 'edit-comments.php' );
+            remove_menu_page( 'plugins.php' );
+            remove_menu_page( 'tools.php' );
+        }
     }
 
     function register_assets() {
@@ -53,7 +59,41 @@ class StarterSite extends TimberSite {
     }
 
 	function register_post_types() {
-		//this is where you can register custom post types
+        $event_labels = array(
+            'name'               => _x( 'Events', 'post type general name', 'modular' ),
+            'singular_name'      => _x( 'Event', 'post type singular name', 'modular' ),
+            'menu_name'          => _x( 'Events', 'admin menu', 'modular' ),
+            'name_admin_bar'     => _x( 'Event', 'add new on admin bar', 'modular' ),
+            'add_new'            => _x( 'Add New', 'event', 'modular' ),
+            'add_new_item'       => __( 'Add New Event', 'modular' ),
+            'new_item'           => __( 'New Event', 'modular' ),
+            'edit_item'          => __( 'Edit Event', 'modular' ),
+            'view_item'          => __( 'View Event', 'modular' ),
+            'all_items'          => __( 'All Events', 'modular' ),
+            'search_items'       => __( 'Search Events', 'modular' ),
+            'parent_item_colon'  => __( 'Parent Events:', 'modular' ),
+            'not_found'          => __( 'No events found.', 'modular' ),
+            'not_found_in_trash' => __( 'No events found in Trash.', 'modular' )
+        );
+
+        $event_args = array(
+            'labels'             => $event_labels,
+            'description'        => __( 'Description.', 'modular' ),
+            'public'             => true,
+            'publicly_queryable' => false,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'query_var'          => true,
+            'rewrite'            => array( 'slug' => 'event' ),
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => 20,
+            'menu_icon'           => 'dashicons-calendar',
+            'supports'           => array( 'title', 'editor' )
+        );
+
+        register_post_type( 'event', $event_args );
 	}
 
 	function register_taxonomies() {
