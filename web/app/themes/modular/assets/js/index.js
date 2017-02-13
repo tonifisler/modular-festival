222b7bca486f5a019b26fef7cb979c6bdeeb76d9
Toni Fisler <mail@tonifisler.ch>
2017-02-13 18:57:04 +0100

    enable scrollspy and fancy navbar

--- web/app/themes/modular/assets/js/index.js
+++ web/app/themes/modular/assets/js/index.js
@@ -1,2 +1,10 @@
 // You will use that file to import all your scripts
 // Ex: import gallery from './gallery';
+
+/* eslint-disable */
+(function(){
+  $(document).on('ready', function() {
+    $('body').scrollspy({ target: '#main-navbar' });
+  });
+}());
+/* eslint-enable */
