647126a8df40516b2cbcf55c4420edfad55c179d
Toni Fisler <mail@tonifisler.ch>
2017-02-27 19:39:41 +0100

    setup a cool menu resizing with scroll

--- web/app/themes/modular/assets/js/index.js
+++ web/app/themes/modular/assets/js/index.js
@@ -4,7 +4,30 @@
 /* eslint-disable */
 (function(){
   $(document).on('ready', function() {
-    $('body').scrollspy({ target: '#main-navbar' });
+    const $body = $('body');
+    const $logo = $('.navbar-brand');
+    const baseHeight = 133;
+
+    // scrollspy for menu
+    $body.scrollspy({ target: '#main-navbar' });
+
+    // shrink menu size on scroll
+    let isSmallMenu = false;
+    $(document).on('scroll', function() {
+      window.requestAnimationFrame(function(){
+        const positionTop = $body.scrollTop();
+        if (positionTop > baseHeight && !isSmallMenu) {
+          toggleMenuSize();
+        } else if(positionTop < baseHeight && isSmallMenu) {
+          toggleMenuSize();
+        }
+      });
+    });
+
+    const toggleMenuSize = function() {
+      $logo.toggleClass('scrolled');
+      isSmallMenu = !isSmallMenu;
+    };
   });
 }());
 /* eslint-enable */
